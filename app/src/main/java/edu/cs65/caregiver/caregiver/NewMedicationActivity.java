b7834d8d78fc05595a13617c074bb2ab65a195d6
Matt McFarland <mcfarland.mattr@gmail.com>
2016-05-17 07:38:17 -0400

    added caregiver dashboard activity and new medication activity

---
+++ app/src/main/java/edu/cs65/caregiver/caregiver/NewMedicationActivity.java
@@ -0,0 +1,13 @@
+package edu.cs65.caregiver.caregiver;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewMedicationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_medication);
+    }
+}
