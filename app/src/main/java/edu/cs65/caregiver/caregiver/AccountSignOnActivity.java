3fcb8b54e3c0c12d6c4d017c391da1940c122712
user-don <don@staynermail.com>
2016-05-16 23:10:48 -0400

    Initial commit

---
+++ app/src/main/java/edu/cs65/caregiver/caregiver/AccountSignOnActivity.java
@@ -0,0 +1,13 @@
+package edu.cs65.caregiver.caregiver;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AccountSignOnActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_sign_on);
+    }
+}
