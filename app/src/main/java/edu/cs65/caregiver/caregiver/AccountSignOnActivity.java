330f1c898e6fc60deb41eb7c32e42456cb08a65e
seanoh <sean.j.oh.17@dartmouth.edu>
2016-05-23 15:18:09 -0400

    changing theme from black/white to blue

--- app/src/main/java/edu/cs65/caregiver/caregiver/AccountSignOnActivity.java
+++ app/src/main/java/edu/cs65/caregiver/caregiver/AccountSignOnActivity.java
@@ -6,30 +6,99 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
 public class AccountSignOnActivity extends Activity {
 
-    private EditText username;
-    private EditText password;
+    private EditText userText;
+    private EditText passText;
+    private String username;
+    private String password;
+    private boolean valid;
+    private int accntType;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_sign_on);
 
-        username = (EditText) findViewById(R.id.username);
-        password = (EditText) findViewById(R.id.password);
+        userText = (EditText) findViewById(R.id.username);
+        passText = (EditText) findViewById(R.id.password);
+
+        valid = false;
+        accntType = -1;
     }
 
     // If the user would like to sign into an existing account
     public void onLogIn (View v) {
+        username = userText.getText().toString();
+        password = passText.getText().toString();
 
+        Intent signUpIntent2 = new Intent(getApplicationContext(), CareRecipientActivity.class);
+        signUpIntent2.putExtra("username",username);
+        signUpIntent2.putExtra("password",password);
+
+        startActivity(signUpIntent2);
+
+        if (username.equals("") || password.equals("")){
+            displayToast("Please enter both an email and password");
+        } else {
+            //check to see if that account is valid
+            valid = true;
+            accntType = 0;
+
+            //if not, available, display toast saying to put in a valid account
+            if (valid) {
+                if (accntType == 0){
+                    Intent signUpIntent = new Intent(getApplicationContext(), CareGiverActivity.class);
+                    signUpIntent.putExtra("username",username);
+                    signUpIntent.putExtra("password",password);
+
+                    startActivity(signUpIntent);
+                } else if (accntType == 1){
+                    Intent signUpIntent = new Intent(getApplicationContext(), CareRecipientActivity.class);
+                    signUpIntent.putExtra("username",username);
+                    signUpIntent.putExtra("password",password);
+
+                    startActivity(signUpIntent);
+                }
+            } else {
+                displayToast("That account doesn't exist. Please try again");
+            }
+        }
     }
 
     // If the user would like to create a new account
     public void onSignUp (View v){
-        Intent signUpIntent = new Intent(getApplicationContext(), NewAccountSignUp.class);
-        signUpIntent.putExtra("username",username.getText().toString());
-        signUpIntent.putExtra("password",password.getText().toString());
-        startActivity(signUpIntent);
+        username = userText.getText().toString();
+        password = passText.getText().toString();
+
+        Intent signUpIntent2 = new Intent(getApplicationContext(), NewAccountSignUp.class);
+        signUpIntent2.putExtra("username",username);
+        signUpIntent2.putExtra("password",password);
+
+        startActivity(signUpIntent2);
+
+        if (username.equals("") || password.equals("")){
+            displayToast("Please enter both an email and password");
+        } else {
+            //check to see if that account is available
+            valid = true;
+
+            //if available, start signup activity else display toast saying to choose a new username
+            if (valid) {
+                Intent signUpIntent = new Intent(getApplicationContext(), NewAccountSignUp.class);
+                signUpIntent.putExtra("username",username);
+                signUpIntent.putExtra("password",password);
+
+                startActivity(signUpIntent);
+            } else {
+                displayToast("Account taken. Please use a new email.");
+            }
+        }
+    }
+
+    public void displayToast(String message){
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
     }
 }
