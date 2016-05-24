a101ba5e1a6fc1017f9dd96f43bdc86b4997d888
ellenpli <li.ellen.p@gmail.com>
2016-05-24 10:10:55 -0400

    fixed sorting meds by time

--- app/src/main/java/edu/cs65/caregiver/caregiver/CareRecipientActivity.java
+++ app/src/main/java/edu/cs65/caregiver/caregiver/CareRecipientActivity.java
@@ -9,9 +9,13 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import java.sql.Time;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.TimeZone;
 
 import edu.cs65.caregiver.caregiver.model.CareGiver;
 import edu.cs65.caregiver.caregiver.model.MedicationAlert;
@@ -50,10 +54,12 @@ public class CareRecipientActivity extends ListActivity {
         }
 
         getDayOfWeek();
+        createTestMeds();
 
         // Get medication alerts and checkin time
         mMedicationAlerts = mReceiver.mAlerts;
         mCheckInTime = mReceiver.mCheckIntime;
+
 
         setUpAdapter();
     }
@@ -99,8 +105,13 @@ public class CareRecipientActivity extends ListActivity {
 
     public void setUpAdapter() {
         ArrayList<MedicationAlert> todaysMeds = getMedsForToday();
-
         sortedMeds = getGroupedMeds(todaysMeds);
+
+        for (int i = 0; i < sortedMeds.size(); i++) {
+            System.out.println("sortedMeds[" + i + "] label - " + sortedMeds.get(i).label);
+            System.out.println("sortedMeds[" + i + "] meds - " + sortedMeds.get(i).meds);
+            System.out.println("sortedMeds[" + i + "] time - " + sortedMeds.get(i).time);
+        }
 
         listValues = new ArrayList<String>();
         listValues.add("10am");
@@ -120,7 +131,7 @@ public class CareRecipientActivity extends ListActivity {
 
         for (int i = 0; i < mMedicationAlerts.size(); i++) {
             int[] alertDays = mMedicationAlerts.get(i).mAlertDays;
-            if (alertDays[7] != 0 || alertDays[dayIndex] != 0) {
+            if (alertDays[dayIndex] != 0) {
                 medsForToday.add(mMedicationAlerts.get(i));
             }
         }
@@ -131,26 +142,28 @@ public class CareRecipientActivity extends ListActivity {
     public ArrayList<MedEntry> getGroupedMeds(ArrayList<MedicationAlert> todaysMeds) {
         ArrayList<MedEntry> sortedMeds = new ArrayList<>();
 
-        for (int i = 0; i < todaysMeds.size(); i++) {
-            for (int j = i+1; j < todaysMeds.size(); j++) {
-                // automatically add first medication
-                if (i == 0) {
-                    MedEntry newEntry = new MedEntry(todaysMeds.get(i).mTime.toString(),
-                            todaysMeds.get(i).mMedications, todaysMeds.get(i).mTime);
-                    sortedMeds.add(newEntry);
-                }
-                // compare medication alert times
-                if (todaysMeds.get(i).compareTo(todaysMeds.get(j)) != 0) {
-                    MedEntry entry = new MedEntry(todaysMeds.get(j).mTime.toString(),
-                            todaysMeds.get(j).mMedications, todaysMeds.get(j).mTime);
-                    sortedMeds.add(entry);
-                }
-                // if found duplicate time, append medications to existing MedEntry object
-                // associated with that time
-                else {
-                    for (int k = 0; k < todaysMeds.get(j).mMedications.size(); k++) {
-                        sortedMeds.get(i).addMedToEntry(todaysMeds.get(j).mMedications.get(k));
-                    }
+        for (int i = 0; i < todaysMeds.size()-1; i++) {
+            int j = i+1;
+
+            // automatically add first medication
+            if (i == 0) {
+                MedEntry newEntry = new MedEntry(todaysMeds.get(i).mTime.toString(),
+                        todaysMeds.get(i).mMedications, todaysMeds.get(i).mTime);
+                sortedMeds.add(newEntry);
+            }
+
+            // compare medication alert times
+            if (todaysMeds.get(i).compareTo(todaysMeds.get(j)) != 0) {
+                MedEntry entry = new MedEntry(todaysMeds.get(j).mTime.toString(),
+                        todaysMeds.get(j).mMedications, todaysMeds.get(j).mTime);
+                sortedMeds.add(entry);
+            }
+
+            // if found duplicate time, append medications to existing MedEntry object
+            // associated with that time
+            else {
+                for (int k = 0; k < todaysMeds.get(j).mMedications.size(); k++) {
+                    sortedMeds.get(i).addMedToEntry(todaysMeds.get(j).mMedications.get(k));
                 }
             }
         }
@@ -158,6 +171,61 @@ public class CareRecipientActivity extends ListActivity {
         return sortedMeds;
     }
 
+    public void createTestMeds() {
+        Time time1 = new Time(10, 0, 0);
+        Time time2 = new Time(15, 15, 0);
+        Time time3 = new Time(10, 0, 0);
+
+        String name1 = "Test1";
+        String name2 = "Test2";
+        String name3 = "Test3";
+
+        int[] days1 = new int[7];
+        days1[0] = 0;
+        days1[1] = 0;
+        days1[2] = 1;
+        days1[3] = 0;
+        days1[4] = 0;
+        days1[5] = 0;
+        days1[6] = 1;
+
+        int[] days2 = new int[7];
+        days2[0] = 0;
+        days2[1] = 0;
+        days2[2] = 1;
+        days2[3] = 0;
+        days2[4] = 0;
+        days2[5] = 0;
+        days2[6] = 1;
+
+        int[] days3 = new int[7];
+        days3[0] = 1;
+        days3[1] = 1;
+        days3[2] = 1;
+        days3[3] = 1;
+        days3[4] = 1;
+        days3[5] = 1;
+        days3[6] = 1;
+
+        ArrayList<String> meds1 = new ArrayList<>();
+        meds1.add("Tylenol");
+        meds1.add("Motrin");
+
+        ArrayList<String> meds2 = new ArrayList<>();
+        meds2.add("Benedryl");
+        meds2.add("Mucinex");
+
+        ArrayList<String> meds3 = new ArrayList<>();
+        meds3.add("Valium");
+
+        MedicationAlert medAlert1 = new MedicationAlert(name1, time1, days1, meds1);
+        MedicationAlert medAlert2 = new MedicationAlert(name2, time2, days2, meds2);
+        MedicationAlert medAlert3 = new MedicationAlert(name3, time3, days3, meds3);
+
+        mReceiver.addAlert(medAlert1);
+        mReceiver.addAlert(medAlert2);
+        mReceiver.addAlert(medAlert3);
+    }
 
 
     public class MedEntry {
