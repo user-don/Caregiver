9cd8ed4735405660a84eb0367a0939fc9bd16ec8
Matt McFarland <mcfarland.mattr@gmail.com>
2016-05-25 00:46:50 -0400

    pulling don's updated backed

--- backend/src/main/java/edu/cs65/caregiver/backend/CreateAccountServlet.java
+++ backend/src/main/java/edu/cs65/caregiver/backend/CreateAccountServlet.java
@@ -29,37 +29,35 @@ public class CreateAccountServlet extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         //super.doPost(req, resp);
 
-        resp.setHeader("result", "successful_account_register");
-
 //        // Here we are passed an email and password for creating a new account.
-//        String email = req.getParameter("email").trim();
-//        String password = req.getParameter("password");
-//        String regId = req.getParameter("registrationId");
-//        String caregiverJson = req.getParameter("caregiver");
-//        String hashedPw = computeMD5Hash(password);
-//        //AccountsDatastore.add(email, hashedPw);
-//        AccountObject account = new AccountObject(email, hashedPw);
-//
-//        // get registration record associated with the id
-//        RegistrationRecord record =
-//                ofy().load().type(RegistrationRecord.class).filter("regId", regId).first().now();
-//        record.setRole("caregiver");
-//        account.addRegistration(record);
-//        if (ofy().load().type(AccountObject.class).filter("email", email).first().now() == null) {
-//            // does not yet exist, create account
-//            ofy().save().entity(account).now();
-//            // create the caregiver object to be stored in the database
-//            CaregiverObject co = new CaregiverObject();
-//            co.setData(caregiverJson);
-//            co.setEmail(email);
-//            ofy().save().entity(co).now();
-//            resp.getWriter().write("result: successful account register");
-//            resp.setHeader("result", "successful_account_register");
-//        } else {
-//            // throw an error toast message back to the client.
-//            // TODO: Send message: account already exists
-//            resp.setHeader("result", "failed_account_exists");
-//        }
+        String email = req.getParameter("email").trim();
+        String password = req.getParameter("password");
+        String regId = req.getParameter("registrationId");
+        String caregiverJson = req.getParameter("caregiver");
+        String hashedPw = computeMD5Hash(password);
+        //AccountsDatastore.add(email, hashedPw);
+        AccountObject account = new AccountObject(email, hashedPw);
+
+        // get registration record associated with the id
+        RegistrationRecord record =
+                ofy().load().type(RegistrationRecord.class).filter("regId", regId).first().now();
+        record.setRole("caregiver");
+        account.addRegistration(record);
+        if (ofy().load().type(AccountObject.class).filter("email", email).first().now() == null) {
+            // does not yet exist, create account
+            ofy().save().entity(account).now();
+            // create the caregiver object to be stored in the database
+            CaregiverObject co = new CaregiverObject();
+            co.setData(caregiverJson);
+            co.setEmail(email);
+            ofy().save().entity(co).now();
+            resp.getWriter().write("result: successful account register");
+            resp.setHeader("result", "successful_account_register");
+        } else {
+            // throw an error toast message back to the client.
+            // TODO: Send message: account already exists
+            resp.setHeader("result", "failed_account_exists");
+        }
 
 
     }
