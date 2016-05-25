c77951a51e6bc1aeabfa4c8db1af7882a7580c32
ellenpli <li.ellen.p@gmail.com>
2016-05-25 11:37:49 -0400

    Gcm and message class

---
+++ app/src/main/java/edu/cs65/caregiver/caregiver/model/RecipientToCareGiverMessage.java
@@ -0,0 +1,41 @@
+package edu.cs65.caregiver.caregiver.model;
+
+import com.google.appengine.repackaged.com.google.gson.Gson;
+
+import java.util.ArrayList;
+
+/**
+ * Created by ellenli on 5/25/16.
+ */
+public class RecipientToCareGiverMessage {
+
+    int CHECKIN = 0;
+    int MED_TAKEN = 1;
+    int MED_NOT_TAKEN = 2;
+    int HELP = 3;
+
+    public int messageType;
+    public long time;
+    public ArrayList<String> medAlertNames;
+
+    public RecipientToCareGiverMessage(int messageType, ArrayList<String> alertNames, long time) {
+        this.messageType = messageType;
+        this.medAlertNames = alertNames;
+        this.time = time;
+    }
+
+    public String selfToString() {
+        Gson gson = new Gson();
+        return gson.toJson(this);
+    }
+
+    public void selfFromString(String string) {
+        Gson gson = new Gson();
+        RecipientToCareGiverMessage recData = gson.fromJson(string, RecipientToCareGiverMessage.class);
+
+        this.messageType = recData.messageType;
+        this.time = recData.time;
+        this.medAlertNames = recData.medAlertNames;
+    }
+
+}
