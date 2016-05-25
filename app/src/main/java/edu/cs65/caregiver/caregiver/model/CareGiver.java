38b58b24ee6ea826648d23b9f8c1f3b0f0c952ce
Matt McFarland <mcfarland.mattr@gmail.com>
2016-05-25 11:37:25 -0400

    trying to figure out data in CareGiver

--- app/src/main/java/edu/cs65/caregiver/caregiver/model/CareGiver.java
+++ app/src/main/java/edu/cs65/caregiver/caregiver/model/CareGiver.java
@@ -26,12 +26,19 @@ public class CareGiver {
     }
 
     public Recipient getRecipient(String _name) {
-        for (Recipient recipient : mRecipients) {
-            if (recipient.mName.equals(_name))
-                return recipient;
+        for (int i = 0; i < mRecipients.size(); i++) {
+            if (mRecipients.get(i).mName.equals(_name))
+                return mRecipients.get(i);
         }
 
         return null;
+    }
+
+    public void setAlert(String recipient_name, MedicationAlert alert) {
+        for (int i = 0; i < mRecipients.size(); i++) {
+            if (mRecipients.get(i).mName.equals(recipient_name))
+                mRecipients.get(i).addAlert(alert);
+        }
     }
 
     public void deleteRecipient(String name) {
